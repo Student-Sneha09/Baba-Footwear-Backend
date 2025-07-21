aebec3b32178344d853b362036fd6f06b6cfd931
Student-Sneha09 <sg23661352@gmail.com>
2025-07-21 23:15:12 +0530

    fixed command class in createsu.py

--- shop/management/commands/createsu.py
+++ shop/management/commands/createsu.py
@@ -1,12 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
+
 class Command(BaseCommand):
-    help = 'Create a superuser if none exists'
+    help = "Create a superuser if it doesn't exist"
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "admin123")
+        username = "admin"
+        email = "admin@example.com"
+        password = "admin123"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS("Superuser created."))
         else:
             self.stdout.write(self.style.WARNING("Superuser already exists."))
