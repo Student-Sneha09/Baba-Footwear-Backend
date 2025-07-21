fa6131cafe5ea02d72b4d546c88bfa0cbbee1818
Student-Sneha09 <sg23661352@gmail.com>
2025-07-21 18:49:18 +0530

    Pages Added

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
+from django.contrib import messages 
 
 # Create your views here.
-def index(request):
+def home(request):
     products= Product.objects.all()
     return render(request, 'shop/index.html', {'products':products})
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name=request.POST["name"]
+        whatsapp=request.POST["whatsapp"]
+        email=request.POST["email"]
+        reason=request.POST["reason"]
+        messages.success(request,"Submitted Successfully! Continue Shopping.")
+        return redirect('home')
+    return render(request, 'shop/contact.html')
