a5330fbaa7b5854ba04dd7071f011ae125881f86
Student-Sneha09 <sg23661352@gmail.com>
2025-07-21 21:15:37 +0530

    prepared for render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddeleware',
+    'whitenoise.middleware.WhiteNoiseMiddleeware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
