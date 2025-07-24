fe3adbc2476c44dee7beba8174306b7d5aff69c2
Student-Sneha09 <sg23661352@gmail.com>
2025-07-25 02:51:11 +0530

    fix cloudinary credential

--- backend/settings.py
+++ backend/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
