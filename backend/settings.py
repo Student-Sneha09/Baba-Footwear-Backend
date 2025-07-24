f17d47ae49a7395bbbcb8a091b88133048c5f4b9
Student-Sneha09 <sg23661352@gmail.com>
2025-07-25 03:55:09 +0530

    trying best to fix error

--- backend/settings.py
+++ backend/settings.py
@@ -143,7 +143,7 @@ import cloudinary.uploader
 import cloudinary.api
 
 cloudinary.config( 
-  cloud_name = os.environ.get('CLOUD_NAME'), 
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
   api_key = os.environ.get('CLOUDINARY_API_KEY'), 
   api_secret = os.environ.get('CLOUDINARY_API_SECRET')
 )
